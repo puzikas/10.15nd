97cfee9679e203ac58f13e3a7913ab9ef8022645
Lukas <lukas.chrap@gmail.com>
2018-10-15 17:01:42 +0300

    10.15

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log('Aplikacija veikia');
+
+var random = document.getElementById('random');
+var next = document.getElementById('next');
+
+
+function random() {
+	console.log('random');
+}
+
+function next() {
+	console.log('next');
+}
